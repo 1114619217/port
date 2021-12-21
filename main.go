af55880207a6ae16d0a1a4479f33daf3c2a20a76
cgf <rick.chen@asiashex.com>
2021-12-21 09:31:01 +0800

    手动计算分页

--- main.go
+++ main.go
@@ -8,10 +8,14 @@ import (
 	"github.com/gocolly/colly/v2"
 	_ "github.com/mattn/go-sqlite3"
 	"log"
+	"math"
+	"net/http"
 	"os"
+	"regexp"
 	"runtime"
+	"strconv"
 	"strings"
-	time "time"
+	"time"
 )
 
 var DbEngin *xorm.Engine
@@ -28,9 +32,14 @@ type Port struct {
 	Route       string `xorm:"varchar(256)" json:"route"`       //航线
 }
 
+type Pages struct {
+	Id  int64  `xorm:"pk autoincr int(11)" json:"id"`
+	Url string `xorm:"varchar(128)"  json:"url"`
+}
+
 func init() {
 	driverName := "sqlite3"
-	dsName := "./port.db"
+	dsName := "./port1220.db"
 	err := errors.New("")
 	DbEngin, err = xorm.NewEngine(driverName, dsName)
 	if nil != err && "" != err.Error() {
@@ -42,7 +51,7 @@ func init() {
 	DbEngin.SetMaxOpenConns(10)
 
 	//自动创建表
-	err = DbEngin.Sync2(new(Port))
+	err = DbEngin.Sync2(new(Port), new(Pages))
 	if err != nil {
 		panic(err)
 	}
@@ -57,41 +66,42 @@ func init() {
 var userAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.107 Safari/537.36"
 var baseURL = "https://www.sofreight.com/ports"
 
-func grab() {
-	c := colly.NewCollector(colly.UserAgent(userAgent))
-	c.OnHTML("#lineList", func(e *colly.HTMLElement) {
-		e.DOM.Find("a").Each(func(i int, d *goquery.Selection) {
-			href, _ := d.Attr("href")
-			pageChan <- href
-		})
-	})
-	e := c.Visit(baseURL)
-	if e != nil {
-		panic(e)
-	}
-}
-
 func grabPage(route string) {
 	c := colly.NewCollector(colly.UserAgent(userAgent))
+	c.WithTransport(&http.Transport{DisableKeepAlives: true})
 	c.OnHTML("#portlist", func(e *colly.HTMLElement) {
 		e.DOM.Find("table").Find("tbody").Find("tr").
 			Find("td").Find("a").Each(func(i int, d *goquery.Selection) {
 			href, _ := d.Attr("href")
 			portChan <- href
 		})
-		d := e.DOM.Find("#portpage")
-		if nil != d {
-			d.Find("a").Each(func(i int, a *goquery.Selection) {
-				href, _ := a.Attr("href")
-				if href != "" && strings.LastIndex(href, "https") > -1 {
-					pageChan <- href
-				}
-			})
-		}
 	})
 	e := c.Visit(route)
 	if e != nil {
 		log.Fatalln(e)
+	}
+}
+
+func allPage() {
+	c := colly.NewCollector(colly.UserAgent(userAgent))
+	c.WithTransport(&http.Transport{DisableKeepAlives: true})
+	c.OnHTML("#lineList", func(e *colly.HTMLElement) {
+		e.DOM.Find("a").Each(func(i int, d *goquery.Selection) {
+			href, _ := d.Attr("href")
+			s, _ := d.Html()
+			re := regexp.MustCompile("[0-9]+")
+			nums, _ := strconv.Atoi(re.FindAllString(s, -1)[0])
+			totalpages := int(math.Ceil(float64(nums) / float64(50)))
+			for i := 1; i <= totalpages; i++ {
+				var page = Pages{}
+				page.Url = href + "?page=" + strconv.Itoa(i)
+				_, _ = DbEngin.InsertOne(page)
+			}
+		})
+	})
+	e := c.Visit(baseURL)
+	if e != nil {
+		panic(e)
 	}
 }
 
@@ -105,12 +115,12 @@ func format(name string) (string, string) {
 
 func grabPort(portURL string) {
 	c := colly.NewCollector(colly.UserAgent(userAgent))
+	c.WithTransport(&http.Transport{DisableKeepAlives: true})
 	c.OnHTML("#portMain", func(e *colly.HTMLElement) {
 		e.DOM.Find("table").Find("tbody").Each(func(i int, tr *goquery.Selection) {
 			tds := tr.Find("td[class!=label]")
 			var port = Port{}
 			tds.Each(func(i int, d *goquery.Selection) {
-				fmt.Println(d.Text())
 				if i == 0 {
 					//港口名称
 					fullName := d.Text()
@@ -140,28 +150,39 @@ func grabPort(portURL string) {
 					port.Route = route
 				}
 			})
-			_, _ = DbEngin.InsertOne(port)
+			insertChan <- port
+			//_, _ = DbEngin.InsertOne(port)
 		})
 	})
 	e := c.Visit(portURL)
 	if e != nil {
-		log.Fatalln(e)
+		fmt.Println(e)
 	}
 }
 
-var pageChan = make(chan string, 400)
+func insertPort(port Port) {
+	_, _ = DbEngin.InsertOne(port)
+}
 
 var portChan = make(chan string, 1000)
+var insertChan = make(chan Port, 1000)
 
 func main() {
 	runtime.GOMAXPROCS(runtime.NumCPU())
-	go grab()
+	//go allPage()
+	go func() {
+		pg := make([]Pages, 0)
+		_ = DbEngin.Find(&pg)
+		for _, u := range pg {
+			grabPage(u.Url)
+		}
+	}()
 	for {
 		select {
-		case route := <-pageChan:
-			go grabPage(route)
 		case p := <-portChan:
 			grabPort(p)
+		case i := <-insertChan:
+			go insertPort(i)
 		case <-time.After(time.Minute * 100):
 			fmt.Println("超时退出...")
 			os.Exit(1)
